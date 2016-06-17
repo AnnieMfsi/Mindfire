74250bfc80875ca441fc67165d6f97dced16b63e
AnnieMfsi <annapurnaa@mindfiresolutions.com>
2016-06-17 11:34:45 +0530

    path of background image changed

--- Profile_App/config/Constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define("IMAGEPATH", "profilePicture/", true);
-	define("DBHOST", "localhost", true);
-	define("DBUSER", "root", true);
-	define("DBPASSWORD", "mindfire", true);
-	define("DBNAME", "RegistrationInfo", true);
-?>
